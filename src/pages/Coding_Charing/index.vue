6267cee026fe4e457b746a805eeb6ee3cc9578ec
Followinsider <ll3670374919@163.com>
2022-10-31 14:32:57 +0800

    初步规划头像和主页界面

--- src/pages/Coding_Charing/index.vue
+++ src/pages/Coding_Charing/index.vue
@@ -2,85 +2,105 @@
 	<!-- 导航栏的粘性布局后续还需改善 -->
 	<div class="scroll" @scroll="handleScroll">
 		<header :class="{visiable:!isMoveDown,'blog-header py-3':true}">
-		<!-- <header class="blog-header py-3"> -->
-			<div class="row flex-nowrap justify-content-between align-items-center">
-				<div class="col-4 pt-1">
-					<a class="text-muted a-distance" href="#">Coding知识共享</a>
-					<a class="text-muted a-distance" href="#">师兄师姐说</a>
-				</div>
-				
-				<div class="col-4 d-flex justify-content-end align-items-center">
-					<div class="create_login">
-						<el-dropdown split-button type="primary" @click="toEdit">
-						创作者中心
-							<el-dropdown-menu slot="dropdown">
-								<el-dropdown-item>我的中心</el-dropdown-item>
-							</el-dropdown-menu>
-						</el-dropdown>
-						<el-button type="primary" plain class="login" @click="login_register">登录 | 注册
-						</el-button>
-					</div>
-					<!-- <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a> -->
-				</div>
-			</div>
-		</header>
-		<div :class="{'view-nav':isMoveDown,'top':!isMoveDown,'container':true}">
-			<div class="nav-scroller py-1 mb-2">
-				<nav class="nav d-flex justify-content-between">
-					<a class="p-2 text-muted" href="#">综合</a>
-					<a class="p-2 text-muted" href="#">前端</a>
-					<a class="p-2 text-muted" href="#">后端</a>
-					<a class="p-2 text-muted" href="#">移动端</a>
-					<a class="p-2 text-muted" href="#">算法</a>
-					<a class="p-2 text-muted" href="#">UI设计</a>
-					<a class="p-2 text-muted" href="#">校内课程</a>
-				</nav>
-			</div>
-		</div>
+            <div class="row flex-nowrap justify-content-between align-items-center">
+                <div class="col-4 pt-1">
+                    <el-button type="primary" icon="el-icon-share" @click="content = true">Coding知识共享</el-button>
+                    <el-button type="success" icon="el-icon-chat-dot-square" @click="content = false">师兄师姐说</el-button>
+                </div>
+                
+                <div class="col-4 d-flex justify-content-end align-items-center">
+                    <div class="create_login">
+                        <el-dropdown split-button type="primary" @click="toEdit">
+                        创作者中心
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item>我的中心</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
+                        <el-button type="primary" plain class="login" @click="login_register" v-show="!isLogin">登录 | 注册
+                        </el-button>
+                        <el-dropdown @command="handleCommand">
+                            <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" v-show="isLogin" class="avatar"></el-avatar>
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item icon="el-icon-plus" command="user">我的主页</el-dropdown-item>
+                                <el-dropdown-item icon="el-icon-user" command="logout">退出登录</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
+                    </div>
+                </div>
+            </div>
+            
+        </header>
+        
+        <div :class="{'view-nav':isMoveDown,'top':!isMoveDown,'container':true}">
+            <div class="nav-scroller py-1 mb-2">
+                <nav class="nav d-flex justify-content-between">
+                    <a class="p-2 text-muted" href="#">综合</a>
+                    <a class="p-2 text-muted" href="#">前端</a>
+                    <a class="p-2 text-muted" href="#">后端</a>
+                    <a class="p-2 text-muted" href="#">移动端</a>
+                    <a class="p-2 text-muted" href="#">算法</a>
+                    <a class="p-2 text-muted" href="#">UI设计</a>
+                    <a class="p-2 text-muted" href="#">校内课程</a>
+                </nav>
+            </div>
+        </div>
 
 		<main role="main" class="container">
-			<Content/>
+			<Content v-if="content"/>
+			<Friends v-else/>
 		</main>
 
 		<!-- 知识共享最后给一句鸡汤 -->
 		<footer class="blog-footer">
+			<p>哈哈哈哈哈哈哈</p>
 			<p>
-			哈哈哈哈哈哈哈
-			</p>
-			<p>
-				<a href="#">回到顶部</a>
+				<a href="javascript:void(0)">回到顶部</a>
 			</p>
 		</footer>
 	</div>
 </template>
 
 <script>
-import Content from './Content.vue'
+import Content from '../../components/Content.vue'
+import Friends from '../../components/Friends.vue'
 export default {
 	name: "Coding-Charing",
 	data() {
 		return {
 			isMoveDown: true,
 			oldScrollTop: 0,
+			content: true,
+			isLogin: false 
 		}
 	},
-	components: {Content},
+	components: {Content, Friends, },
 	methods: {
-		toEdit() {
-			this.$router.push('edit')
-		},
-		login_register() {
-			this.$router.push('login')
-		},
+		// 控制滚动轮
 		handleScroll(e) {
 			let scrollStep = e.target.scrollTop - this.oldScrollTop;
 			this.oldScrollTop = e.target.scrollTop;
 			if (scrollStep > 0) {
-				this.isMoveDown = true;
+				this.isMoveDown = true
 			}else {
-				this.isMoveDown = false;
+				this.isMoveDown = false
 			}
-			
+		},
+		// 编辑内容
+		toEdit() {
+			this.$router.push('edit')
+		},
+		// 登录和注册相关
+		login_register() {
+			// this.$router.push('login')
+			this.isLogin = true
+		},
+		// 头像下拉框的选择
+		handleCommand(command) {
+			if (command === 'user') {
+				this.$router.push('user')
+			}else if (command === 'logout') {
+				this.isLogin = false
+			}
 		}
 	},
 };
@@ -186,11 +206,6 @@ button:focus {
 	height: 600px;
 }
 
-
-.scroll {
-	overflow: scroll;
-	height: 100vh;
-}
 .visiable {
 	/* position: fixed;
 	top: 0;
@@ -220,5 +235,25 @@ button:focus {
 
 .text-muted {
 	line-height: 40px;
+}
+
+.row {
+	margin: 0;
+}
+.create_login {
+	display: flex;
+	justify-content: center;
+}
+.el-dropdown {
+	vertical-align: 0;
+}
+
+.avatar {
+	margin-left: 12px;
+}
+
+.scroll {
+	overflow: scroll;
+	height: 100vh;
 }
 </style>
